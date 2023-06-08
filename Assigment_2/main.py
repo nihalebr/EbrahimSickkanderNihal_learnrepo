35529ad327a58fcf0dc50b8be32cbf4f6c04ca22
nihalebr <24320465+nihalebr@users.noreply.github.com>
2023-06-08 09:02:52 +0530

    feat: Assigment2 Completed.

---
+++ Assigment_2/main.py
@@ -0,0 +1,45 @@
+""" Assignment 2 is a function reads the json file and stores it value in dict variable named ex5.
+
+Looping through each element in the list of dict we check for two values to be present in side dict one is name should be `Old Fashioned` other is type `donut` else continue the loop.
+
+if true check for the batter name `Coffee` present in the list of dict then return.
+
+if false sort the list of batters dict based on `id` key. Increment the id by one and set type as `Coffee` , append it to batters dict.
+"""
+
+import json
+
+
+def sortById(e):
+    return e['id']
+
+
+def assignment2():
+    with open('ex5.json', 'r') as jsonFile:
+        if not jsonFile.readline():
+            print('file is empty')
+            return
+        jsonFile.seek(0)
+        ex5 = json.load(jsonFile)
+        jsonFile.close()
+
+    for data in ex5:
+        if (type(data) is not dict):
+            print('Data is not in the type of dict')
+            return
+        if 'Old Fashioned' in data['name'] and 'donut' in data['type']:
+            batters = data['batters']['batter']
+            if any('Coffee' in batter['type'] for batter in batters):
+                print('Type Coffee already present in the batter')
+                return
+            batters.sort(key=sortById)
+            newId = int(batters[-1]['id'])+1
+            batters.append({'id': str(newId), 'type': 'Coffee'})
+            print(f"Added batter type Coffee with id {newId}.")
+
+    with open('ex5.json', 'w') as jsonFile:
+        json.dump(ex5, jsonFile)
+        jsonFile.close()
+
+
+assignment2()
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Ebrahim Sickkander Nihal's Learn Repo
 
-## Assigment 1
+## Assignment 1
 
 - Create a repo under this specified model `yourname_learnrepo`.
-- Create a simple Pyhton File and Push the same to the RemoteRepo.
+- Create a simple Python File and Push the same to the RemoteRepo.
+
+## Assignment 2 
+
+- TL;DR : Create a Python file that will read a JSON file and Update it.
+- Get Sample Data From this [site](https://opensource.adobe.com/Spry/samples/data_region/JSONDataSetSample.html#Example5) (or you can see the [file](./Assigment2.json) ).
+- After collecting the data from the site store the json file in the name of `ex5.json`.
+- Now, Read the JSON file and Store the content of the file in a variable named `ex5` and use `dictionary` as data type.
+- Finally, add a batter named `coffee` for `donut` with name `Old Fashioned` and update the `ex5.json` file.
